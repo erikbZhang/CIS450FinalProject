6572445ccc66acf6a61a10a1c6877901b0d4999b
skhan-07 <126933895+skhan-07@users.noreply.github.com>
2023-05-02 17:44:19 -0400

    edited test files

--- server/__tests__/tests.js
+++ server/__tests__/tests.js
@@ -1,143 +1,278 @@
 const { expect } = require('@jest/globals');
 const supertest = require('supertest');
 const app = require('../server');
-const results = require("./results.json")
+const results = require("./results.json");
 
-test('GET /author/name', async () => {
-  await supertest(app).get('/author/name')
+
+/*app.get('/match', routes.match);
+app.get('/matchAlbum', routes.matchAlbum);
+app.get('/matchArtist', routes.matchArtist);
+app.get('/misMatch', routes.misMatch);
+app.get('/emotionPlaylist/:emotion', routes.emotionPlaylist);
+app.get('/emotionArtists/:emotion', routes.emotionArtists);
+app.get('/emotionAlbums/:emotion', routes.emotionAlbums);
+app.get('/songEmotionScore/:song_title', routes.songEmotionScore);
+app.get('/albumEmotionScore/:album_title', routes.albumEmotionScore);
+app.get('/artistEmotionScore/:artist', routes.artistEmotionScore);*/
+
+
+test('GET /match', async () => {
+  await supertest(app).get('/match')
     .expect(200)
     .then((res) => {
-      expect(res.text).toMatch(/(?!.* John Doe$)^Created by .*$/);
-    });
-});
-
-test('GET /author/pennkey', async () => {
-  await supertest(app).get('/author/pennkey')
-    .expect(200)
-    .then((res) => {
-      expect(res.text).toMatch(/(?!.* jdoe$)^Created by .*$/);
-    });
-});
-
-test('GET /random', async () => {
-  await supertest(app).get('/random')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual({
-        song_id: expect.any(String),
-        title: expect.any(String),
-      });
-    });
-});
-
-test('GET /song/0kN3oXYWWAk1uC0y2WoyOE', async () => {
-  await supertest(app).get('/song/0kN3oXYWWAk1uC0y2WoyOE')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual(results.song)
-    });
-});
-
-test('GET /album/3lS1y25WAhcqJDATJK70Mq', async () => {
-  await supertest(app).get('/album/3lS1y25WAhcqJDATJK70Mq')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual(results.album)
-    });
-});
-
-test('GET /albums', async () => {
-  await supertest(app).get('/albums')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual(results.albums)
-    });
-});
-
-test('GET /album_songs/6AORtDjduMM3bupSWzbTSG', async () => {
-  await supertest(app).get('/album_songs/6AORtDjduMM3bupSWzbTSG')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual(results.album_songs)
-    });
-});
-
-test('GET /top_songs all', async () => {
-  await supertest(app).get('/top_songs')
-    .expect(200)
-    .then((res) => {
-      expect(res.body.length).toEqual(238)
-      expect(res.body[22]).toStrictEqual(results.top_songs_all_22)
-    });
-});
-
-test('GET /top_songs page 3', async () => {
-  await supertest(app).get('/top_songs?page=3')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual(results.top_songs_page_3)
-    });
-});
-
-test('GET /top_songs page 5 page_size 3', async () => {
-  await supertest(app).get('/top_songs?page=5&page_size=3')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual(results.top_songs_page_5_page_size_3)
-    });
-});
-
-test('GET /top_albums all', async () => {
-  await supertest(app).get('/top_albums')
-    .expect(200)
-    .then((res) => {
-      expect(res.body.length).toEqual(12)
-      expect(res.body[7]).toStrictEqual(results.top_albums_all_7)
-    });
-});
-
-test('GET /top_albums page 2', async () => {
-  await supertest(app).get('/top_albums?page=2')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual(results.top_albums_page_2)
-    });
-});
-
-test('GET /top_albums page 5 page_size 1', async () => {
-  await supertest(app).get('/top_albums?page=5&page_size=1')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual(results.top_albums_page_5_page_size_1)
-    });
-});
-
-test('GET /search_songs default', async () => {
-  await supertest(app).get('/search_songs')
-    .expect(200)
-    .then((res) => {
-      expect(res.body.length).toEqual(219)
       expect(res.body[0]).toStrictEqual({
-        song_id: expect.any(String),
-        album_id: expect.any(String),
+        album: expect.any(String),
+        artist: expect.any(String),
+        date: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z$/),
+        distance: expect.any(Number),
         title: expect.any(String),
-        number: expect.any(Number),
-        duration: expect.any(Number),
-        plays: expect.any(Number),
-        danceability: expect.any(Number),
-        energy: expect.any(Number),
-        valence: expect.any(Number),
-        tempo: expect.any(Number),
-        key_mode: expect.any(String),
-        explicit: expect.any(Number),
+        year: expect.any(Number),
       });
     });
 });
 
-test('GET /search_songs filtered', async () => {
-  await supertest(app).get('/search_songs?title=all&explicit=true&energy_low=0.5&valence_low=0.2&valence_high=0.8')
+test('GET /matchAlbum', async () => {
+  await supertest(app).get('/matchAlbum')
     .expect(200)
     .then((res) => {
-      expect(res.body).toStrictEqual(results.search_songs_filtered)
+      expect(res.body[0]).toStrictEqual({
+        album: expect.any(String),
+        artist: expect.any(String),
+        cosine_similarity: expect.any(Number),
+      });
+    });
+});
+
+test('GET /matchArtist', async () => {
+  await supertest(app)
+    .get('/matchArtist')
+    .expect(200)
+    .then((res) => {
+      expect(res.body[0]).toStrictEqual({
+        artist: expect.any(String),
+        distance: expect.any(Number),
+      });
+    });
+});
+
+test('GET /misMatch', async () => {
+  await supertest(app)
+    .get('/misMatch')
+    .expect(200)
+    .then((res) => {
+      expect(res.body[0]).toStrictEqual({
+        album: expect.any(String),
+        artist: expect.any(String),
+        date: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z$/),
+        distance: expect.any(Number),
+        title: expect.any(String),
+        year: expect.any(Number),
+      });
+    });
+});
+
+test('GET /emotionPlaylist/:emotion', async () => {
+  const emotion = 'happy';
+
+  await supertest(app)
+    .get(`/emotionPlaylist/${emotion}`)
+    .expect(200);
+});
+
+test('GET /emotionPlaylist/:emotion', async () => {
+  const emotion = 'afraid';
+
+  await supertest(app)
+    .get(`/emotionPlaylist/${emotion}`)
+    .expect(200);
+});
+
+test('GET /emotionArtists/:emotion', async () => {
+  const emotion = 'sad';
+
+  await supertest(app)
+    .get(`/emotionArtists/${emotion}`)
+    .expect(200);
+});
+
+test('GET /emotionAlbums/:emotion', async () => {
+  const emotion = 'angry';
+
+  await supertest(app)
+    .get(`/emotionAlbums/${emotion}`)
+    .expect(200);
+});
+
+test('GET /emotionAlbums/:emotion returns empty object when no data is found', async () => {
+  await supertest(app).get('/emotionAlbums/nonexistent_emotion')
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toStrictEqual({});
+    });
+});
+
+test('GET /songEmotionScore/:song_title', async () => {
+  const songTitle = 'Blank Space';
+  const expectedResults = {
+    anger_score: expect.any(Number),
+    anticipation_score: expect.any(Number),
+    artist: 'Taylor Swift',
+    disgust_score: expect.any(Number),
+    fear_score: expect.any(Number),
+    joy_score: expect.any(Number),
+    negative_score: expect.any(Number),
+    positive_score: expect.any(Number),
+    sadness_score: expect.any(Number),
+    surprise_score: expect.any(Number),
+    title: 'Blank Space',
+    trust_score: expect.any(Number),
+  };
+
+  await supertest(app)
+    .get(`/songEmotionScore/${songTitle}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body[0]).toStrictEqual(expectedResults);
+    });
+});
+
+test('GET /songEmotionScore/:song_title - returns empty object for non-existent song', async () => {
+  const nonExistentSong = 'This Song Does Not Exist';
+  await supertest(app).get(`/songEmotionScore/${nonExistentSong}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toEqual({});
+    });
+});
+
+test('GET /albumEmotionScore/:album_title', async () => {
+  const albumTitle = 'Lover';
+  const expectedResults = {
+    album: 'Lover',
+    artist: 'Taylor Swift',
+    avg_anger_score: expect.any(Number),
+    avg_anticipation_score: expect.any(Number),
+    avg_disgust_score: expect.any(Number),
+    avg_fear_score: expect.any(Number),
+    avg_joy_score: expect.any(Number),
+    avg_negative_score: expect.any(Number),
+    avg_positive_score: expect.any(Number),
+    avg_sadness_score: expect.any(Number),
+    avg_surprise_score: expect.any(Number),
+    avg_trust_score: expect.any(Number),
+    date: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z$/),
+    year: expect.any(Number)
+  };
+
+  await supertest(app)
+    .get(`/albumEmotionScore/${albumTitle}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body[0]).toStrictEqual(expectedResults);
+    });
+});
+
+test('GET /albumEmotionScore/:album_title - returns empty object for non-existent album', async () => {
+  const nonExistentAlbum = 'This Album Does Not Exist';
+  await supertest(app).get(`/songEmotionScore/${nonExistentAlbum}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toEqual({});
+    });
+});
+
+test('GET /artistEmotionScore/:artist', async () => {
+  const artistName = 'Taylor Swift';
+  const expectedResults = {
+    artist: 'Taylor Swift',
+    avg_anger_score: expect.any(Number),
+    avg_anticipation_score: expect.any(Number),
+    avg_disgust_score: expect.any(Number),
+    avg_fear_score: expect.any(Number),
+    avg_joy_score: expect.any(Number),
+    avg_negative_score: expect.any(Number),
+    avg_positive_score: expect.any(Number),
+    avg_sadness_score: expect.any(Number),
+    avg_surprise_score: expect.any(Number),
+    avg_trust_score: expect.any(Number),
+  };
+
+  await supertest(app)
+    .get(`/artistEmotionScore/${artistName}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body[0]).toStrictEqual(expectedResults);
+    });
+});
+
+test('GET /artistEmotionScore/:artist', async () => {
+  const artistName = 'Ariana Grande';
+  const expectedResults = {
+    artist: 'Ariana Grande',
+    avg_anger_score: expect.any(Number),
+    avg_anticipation_score: expect.any(Number),
+    avg_disgust_score: expect.any(Number),
+    avg_fear_score: expect.any(Number),
+    avg_joy_score: expect.any(Number),
+    avg_negative_score: expect.any(Number),
+    avg_positive_score: expect.any(Number),
+    avg_sadness_score: expect.any(Number),
+    avg_surprise_score: expect.any(Number),
+    avg_trust_score: expect.any(Number),
+  };
+
+  await supertest(app)
+    .get(`/artistEmotionScore/${artistName}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body[0]).toStrictEqual(expectedResults);
+    });
+});
+
+test('GET /artistEmotionScore/:artist', async () => {
+  const artistName = 'Kanye West';
+  const expectedResults = {
+  };
+
+  await supertest(app)
+    .get(`/artistEmotionScore/${artistName}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toEqual(expectedResults);
+    });
+});
+
+test('GET /emotionAlbums/:emotion with non-existing emotion', async () => {
+  const nonExistingEmotion = 'despise';
+  await supertest(app)
+    .get(`/emotionAlbums/${nonExistingEmotion}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toEqual({});
+    });
+});
+
+test('GET /emotionAlbums/:emotion with non-existing emotion', async () => {
+  const nonExistingEmotion = 'antsy';
+  await supertest(app)
+    .get(`/emotionAlbums/${nonExistingEmotion}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toEqual({});
+    });
+});
+
+test('GET /emotionAlbums/:emotion', async () => {
+  const emptyDataEmotion = 'joy';
+
+  await supertest(app)
+    .get(`/emotionAlbums/${emptyDataEmotion}`)
+    .expect(200)
+    .then((res) => {
+      expect(res.body[0]).toEqual({
+        album: expect.any(String),
+        artist: expect.any(String),
+        avg_joy_score: expect.any(Number),
+      });
     });
 });
